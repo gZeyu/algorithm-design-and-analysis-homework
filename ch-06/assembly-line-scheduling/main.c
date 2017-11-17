885bcd6340bee1d31eba79f1db0555d549bf7835
gZeyu <gzy8535@gmail.com>
2017-11-17 17:31:18 +0800

    ex-06-03

--- ch-06/assembly-line-scheduling/main.c
+++ ch-06/assembly-line-scheduling/main.c
@@ -2,8 +2,8 @@
 
 #define N 6
 
-void DPFastestWay(int **a, int **t, int *e, int *x, int n,
-                  int **f, int **l, int *fOut, int *lOut)
+void DPFastestWay(int a[][N], int t[][N], int *e, int *x, int n,
+                  int f[][N], int l[][N], int *fOutPtr, int *lOutPtr)
 {
     int j;
 
@@ -11,39 +11,51 @@ void DPFastestWay(int **a, int **t, int *e, int *x, int n,
     f[1][0] = e[1] + a[1][0];
     for (j = 1; j < n; ++j)
     {
-        if (f[0][j - 1] + a[0][j] <= f[1][j - 1] + t[1][j - 1] + a[1][j])
+        if (f[0][j - 1] + a[0][j] <= f[1][j - 1] + t[1][j - 1] + a[0][j])
         {
             f[0][j] = f[0][j - 1] + a[0][j];
             l[0][j] = 0;
         }
         else
         {
-            f[0][j] = f[1][j - 1] + t[1][j - 1] + a[1][j];
+            f[0][j] = f[1][j - 1] + t[1][j - 1] + a[0][j];
             l[0][j] = 1;
         }
-    }
-    for (j = 1; j < n; ++j)
-    {
-        if (f[1][j - 1] + a[1][j] <= f[0][j - 1] + t[0][j - 1] + a[0][j])
+        if (f[1][j - 1] + a[1][j] <= f[0][j - 1] + t[0][j - 1] + a[1][j])
         {
             f[1][j] = f[1][j - 1] + a[1][j];
-            l[0][j] = 1;
+            l[1][j] = 1;
         }
         else
         {
-            f[1][j] = f[0][j - 1] + t[0][j - 1] + a[0][j];
-            l[0][j] = 0;
+            f[1][j] = f[0][j - 1] + t[0][j - 1] + a[1][j];
+            l[1][j] = 0;
         }
     }
     if (f[0][n - 1] + x[0] <= f[1][n - 1] + x[1])
     {
-        *fOut = f[0][n - 1] + x[0];
-        *lOut = 0;
+        *fOutPtr = f[0][n - 1] + x[0];
+        *lOutPtr = 0;
     }
     else
     {
-        *fOut = f[1][n - 1] + x[1];
-        *lOut = 1;
+        *fOutPtr = f[1][n - 1] + x[1];
+        *lOutPtr = 1;
+    }
+
+    return;
+}
+
+void printStation(int l[][N], int lOut, int n)
+{
+    int i, j;
+
+    i = lOut;
+    printf("line %d, station %d\n", i, n);
+    for (j = n - 1; j > 0; --j)
+    {
+        i = l[i][j];
+        printf("line %d, station %d\n", l[i][j], j);
     }
 
     return;
@@ -51,7 +63,20 @@ void DPFastestWay(int **a, int **t, int *e, int *x, int n,
 
 int main()
 {
-    int a[][N] = {7, 9, 3, 4, 8, 4,
+    
+
+    int a[2][N] = {7, 9, 3, 4, 8, 4,
                   8, 5, 6, 4, 5, 7};
+    int t[2][N] = {2, 3, 1, 3, 4, 3,
+                  2, 1, 2, 2, 1, 2};
+    int e[2] = {2, 4};
+    int x[2] = {3, 2};
+    int n = N;
+    int f[2][N], l[2][N], fOut, lOut;
+
+    DPFastestWay(a, t, e, x, n, 
+                 f, l, &fOut, &lOut);
+    printStation(l, lOut, n);
+
     return 0;
 }
--- ch-06/ex-06-03/main.c
+++ ch-06/ex-06-03/main.c
@@ -1,102 +1,82 @@
 #include <stdio.h>
 
-#define MAX 1024
-void printfArray(const int *A, int n)
+#define N 6
+
+void DPFastestWay(int a[][N], int t[][N], int *e, int *x, int n,
+                  int f[][N], int l[][N], int *fOutPtr, int *lOutPtr)
 {
-    int i;
+    int j;
 
-    for (i = 1; i <= n; ++i)
+    f[0][0] = e[0] + a[0][0];
+    f[1][0] = e[1] + a[1][0];
+    for (j = 1; j < n; ++j)
     {
-        printf("%d", A[i]);
-    }
-    printf("\n");
-
-    return;
-}
-
-void Perm1(int *P, int n, int m) //非字典序
-{
-    int j, tmp;
-
-    if(m == n)
-    {
-        printfArray(P, n);
-    }
-    else
-    {
-        for(j = m; j <= n; ++j)
+        if (f[0][j - 1] + a[0][j] <= f[1][j - 1] + t[1][j - 1] + a[0][j])
         {
-            tmp = P[j];
-            P[j] = P[m];
-            P[m] = tmp;
-            Perm1(P, n, m + 1);
-            tmp = P[j];
-            P[j] = P[m];
-            P[m] = tmp;
+            f[0][j] = f[0][j - 1] + a[0][j];
+            l[0][j] = 0;
+        }
+        else
+        {
+            f[0][j] = f[1][j - 1] + t[1][j - 1] + a[0][j];
+            l[0][j] = 1;
+        }
+        if (f[1][j - 1] + a[1][j] <= f[0][j - 1] + t[0][j - 1] + a[1][j])
+        {
+            f[1][j] = f[1][j - 1] + a[1][j];
+            l[1][j] = 1;
+        }
+        else
+        {
+            f[1][j] = f[0][j - 1] + t[0][j - 1] + a[1][j];
+            l[1][j] = 0;
         }
     }
-
-    return;
-}
-
-void GeneratingPerm1(int *P, int n)
-{
-    int j;
-    for (j = 1; j <= n; ++j)
+    if (f[0][n - 1] + x[0] <= f[1][n - 1] + x[1])
     {
-        P[j] = j;
-    }
-    Perm1(P, n, 1);
-    return;
-}
-
-void Perm12(int *V, int *O, const int *P, int n, int m) //字典序
-{
-    int j;
-
-    if (m > n)
-    {
-        printfArray(O, n);
+        *fOutPtr = f[0][n - 1] + x[0];
+        *lOutPtr = 0;
     }
     else
     {
-        for (j = 1; j <= n; ++j)
-        {
-            if (V[P[j]] == 0)
-            {
-                O[m] = P[j];
-                V[P[j]] = 1;
-                Perm12(V, O, P, n,  m + 1);
-                V[P[j]] = 0;
-            }
-        } 
-
+        *fOutPtr = f[1][n - 1] + x[1];
+        *lOutPtr = 1;
     }
 
     return;
 }
 
-void GeneratingPerm12(int *P, int n)
+void printStation(int l[][N], int lOut, int n)
 {
-    int V[MAX] = {0};
-    int O[MAX];
-    int j;
+    int i, j;
 
-    for (j = 1; j <= n; ++j)
+    i = lOut;
+    printf("line %d, station %d\n", i, n);
+    for (j = n - 1; j > 0; --j)
     {
-        P[j] = j;
+        i = l[i][j];
+        printf("line %d, station %d\n", l[i][j], j);
     }
-    Perm12(V, O, P, n, 1);
-    
+
+    return;
 }
+
 int main()
 {
-    int P[MAX];
     
-    printf("GeneratingPerm1\n");
-    GeneratingPerm1(P, 3);
-    printf("GeneratingPerm12\n");
-    GeneratingPerm12(P, 4);
+
+    int a[2][N] = {7, 9, 3, 4, 8, 4,
+                  8, 5, 6, 4, 5, 7};
+    int t[2][N] = {2, 3, 1, 3, 4, 3,
+                  2, 1, 2, 2, 1, 2};
+    int e[2] = {2, 4};
+    int x[2] = {3, 2};
+    int n = N;
+    int f[2][N], l[2][N], fOut, lOut;
+
+    DPFastestWay(a, t, e, x, n, 
+                 f, l, &fOut, &lOut);
+    printStation(l, lOut, n);
 
     return 0;
 }
